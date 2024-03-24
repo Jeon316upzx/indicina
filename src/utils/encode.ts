044cda3182b1c4919660d7d03796aaa9351202d5
Jeon316upzx <mamasbwoyjeon94@gmail.com>
2024-03-24 21:56:41 +0100

    feat: add encodeUrl function and helper utility functions

---
+++ src/utils/encode.ts
@@ -0,0 +1,39 @@
+export const encodeUrl = (keyLength: number ): string => {
+  let salt = keyLength || 4;
+
+  let result = null;
+  for (let i = 0; i < salt; i++) {
+    result += generateRandomChar();
+  }
+  return result;
+};
+
+const getAllCharacters = (): string[] => {
+  const lowerCaseAlphabets = "abcdefghijklmnopqrstuvwxyz";
+  const upperCaseAlphabets = lowerCaseAlphabets.toUpperCase();
+  const numericalDigits = "0123456789";
+  const chars = (
+    lowerCaseAlphabets +
+    upperCaseAlphabets +
+    numericalDigits
+  ).split("");
+  return shuffle(chars);
+};
+
+const getRandomIndex = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const generateRandomChar = (): string => {
+  const characters = getAllCharacters();
+  const index = getRandomIndex(1, characters.length - 1);
+  return characters[index];
+};
+
+function shuffle(a: string[]) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
