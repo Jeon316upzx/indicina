d60156528fdb2715d0ae89fb474aad7695fd3e51
Jeon316upzx <mamasbwoyjeon94@gmail.com>
2024-03-24 14:54:18 +0100

    types: add a UrlModel type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,14 @@
 export type EnvConfig = {
-    PORT: number;
-    JWT_SECRET: string;
-    DATABASE_URL: string;
-    BASE_URL: string
-  };
+  PORT: number;
+  JWT_SECRET: string;
+  DATABASE_URL: string;
+  BASE_URL: string;
+};
+
+export type UrlModel = {
+  originalUrl: string;
+  shortUrl: string;
+  visits: number;
+  userAgents: string[];
+  geoLocation: string[];
+};
