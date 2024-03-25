e5342a3c705f287d98530740462f6486ea53580b
Jeon316upzx <mamasbwoyjeon94@gmail.com>
2024-03-25 11:05:35 +0100

    fix: update url type from urlType to string

--- src/validators/url.validator.ts
+++ src/validators/url.validator.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import { UrlSchema } from "./schema/urlSchema";
 import { UrlType } from "../types";
 
-export const validateUrl = ({ originalUrl }: UrlType): boolean => {
+export const validateUrl = ( originalUrl : string): boolean => {
   const { value, error } = UrlSchema.validate(originalUrl);
   if (error) throw new Error(`Validation error: ${error.message}`);
   return false;
